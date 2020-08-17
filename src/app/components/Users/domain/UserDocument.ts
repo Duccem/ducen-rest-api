093621d95b97767f0471b117064b46215b390c57
Duccem <josemanuel376@hotmail.com>
2020-08-17 00:03:33 -0400

    Added the types of response documentes

---
+++ src/app/components/Users/domain/UserDocument.ts
@@ -0,0 +1,16 @@
+import { JsonDocument } from "../../../libs/Types/JsonDocument";
+
+export type UserJsonDocument = JsonDocument & {
+	nombres: string;
+	apellidos: string;
+	fecha_nacimiento: string;
+	sexo: string;
+	direccion: string;
+	usuario: string;
+	password: string;
+};
+
+export type AuthJsonDocument = {
+	token: string;
+	user: UserJsonDocument;
+};
