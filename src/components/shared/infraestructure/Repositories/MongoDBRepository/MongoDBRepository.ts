776bb6fb9ff080138ba2c41601e52c77ea3dac15
Duccem <josemanuel376@hotmail.com>
2020-08-29 21:50:33 -0400

    .

--- src/components/shared/infraestructure/Repositories/MongoDBRepository/MongoDBRepository.ts
+++ src/components/shared/infraestructure/Repositories/MongoDBRepository/MongoDBRepository.ts
@@ -7,20 +7,31 @@ import { Nulleable } from "components/shared/domain/Types/Nulleable";
 import { NextFunction, Request, Response } from "express";
 import { QueryMaker } from "../QueryMaker";
 import { MongoDBQueryMaker } from "./MongoDBQueryMaker";
-import { MongoClient, Db } from 'mongodb'
+import { MongoClient, Db, Collection} from 'mongodb'
 
 export class MongoDBRepoitory implements Repository {
     private logger: Logger;
     private query: QueryMaker;
-    private connection: Db;
-    constructor(databse?: any, logger?: Logger){
+    private database: any;
+    constructor(database?: any, logger?: Logger){
         this.logger = logger || new Logger();
         this.query = new MongoDBQueryMaker();
-        this.connection = 
+        this.database = database;
+    }
+
+    private async getConnection(dbName: string): Promise<Collection> {
+        let collection  = await (await MongoClient.connect(this.database.host)).db(this.database.name).collection(dbName);
+        return collection;
     }
 
     public async list<T extends JsonDocument>(model: string, options: ConsulterOptions): Promise<Array<T>>{
-        return new Array<T>();
+        let { conditional, limit, orderField, order, offset, fields  } = this.query.findMany(model,options);
+        let data: Array<T> = await (await this.getConnection(model))
+                                    .find(conditional, fields)
+                                    .skip(offset).limit(limit)
+                                    .sort({[`${orderField}`]: order})
+                                    .toArray()
+        return data;
     }
 
     public async get<T extends JsonDocument>(model: string, id: number | string, options: ConsulterOptions): Promise<Nulleable<T>>{
