923096aa3284b89f9761acaa4fe91fc7df9a602e
Duccem <jose.veliz@bluedott.tech>
2021-01-07 08:53:28 -0400

    Delete the rest api

--- src/apps/RestApi/observers/email.observe.ts
+++
@@ -1,8 +0,0 @@
-import { EmailEventsSubscriber } from "../../../contexts/ClientAttention/Emails/application/EmailEventsSubscriber";
-import Container from "typedi";
-
-
-export function register() {
-	const emailEventSubscriber = Container.get<EmailEventsSubscriber>("EmailSubscriber")
-	return emailEventSubscriber;
-}
